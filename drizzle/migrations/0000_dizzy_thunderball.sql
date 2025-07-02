81372da47c2acde95a58a3d575769b171aa5b671
Edvinas Momkus <edvinasmomkus@gmail.com>
2025-07-02 13:10:01 +0200

    remove migrations files

--- drizzle/migrations/0000_dizzy_thunderball.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `words` (
-	`id` integer PRIMARY KEY NOT NULL,
-	`language` text,
-	`word` text
-);
--- drizzle/migrations/0005_damp_plazm.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE `ch_member` RENAME COLUMN "added" TO "signed";--> statement-breakpoint
-ALTER TABLE `chat` RENAME COLUMN "added" TO "signed";--> statement-breakpoint
-ALTER TABLE `log` RENAME COLUMN "added" TO "signed";--> statement-breakpoint
-ALTER TABLE `message` RENAME COLUMN "added" TO "signed";--> statement-breakpoint
-ALTER TABLE `user` RENAME COLUMN "added" TO "signed";--> statement-breakpoint
-DROP TABLE `words`;
