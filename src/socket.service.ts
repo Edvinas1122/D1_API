d29cb4b1410c4b7cd1039cd7184c8408a63baa8d
Edvinas Momkus <35561374+Edvinas1122@users.noreply.github.com>
2025-07-12 19:44:03 +0000

    type

--- src/socket.service.ts
+++ src/socket.service.ts
@@ -3,8 +3,8 @@ import { OnError } from "./utils/error";
 
 export class SocketUtils extends DB {
 	
-	@OnError()
-	async key(email: string) {
+	// @OnError()
+	async key(email: string): Promise<string> {
 		// @ts-ignore
 		const response = await this.env.Socket.token(email);
 		return response;
